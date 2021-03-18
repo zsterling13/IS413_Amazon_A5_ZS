01aee00c476621022234ef33bdf3b270d85efb73
Zach <zach.sterling18@gmail.com>
2021-03-18 17:49:27 -0600

    Updated database

---
+++ Migrations/20210316025326_New.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IS413_Amazon_A5_ZS.Migrations
+{
+    public partial class New : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
